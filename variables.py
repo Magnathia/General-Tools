631dad4c9cf246ba626c65c7de39f48f8f2ee0e3
Ron Smith <Magnathia@gmail.com>
2024-12-13 12:15:27 -0800

    Remove variables.py from tracking

--- variables.py
+++
@@ -1,6 +0,0 @@
-DOMAIN = "yourdomain.com"
-USERS_OU = "OU=PMEL Users,DC=yourdomain,DC=com"
-EXCLUDE_OUS = [
-    "OU=SysAdmin,OU=PMEL Users,DC=yourdomain,DC=com",
-    "OU=Supplementary,OU=PMEL Users,DC=yourdomain,DC=com"
-]
