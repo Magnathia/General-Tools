b4b7a1564dc92526a34c551ac2fcf5b3c8be9a79
Ron Smith <Magnathia@gmail.com>
2024-12-13 12:23:00 -0800

    updated launch file and AD Functions

---
+++ variables.py
@@ -0,0 +1,6 @@
+DOMAIN = "yourdomain.com"
+USERS_OU = "OU=PMEL Users,DC=yourdomain,DC=com"
+EXCLUDE_OUS = [
+    "OU=SysAdmin,OU=PMEL Users,DC=yourdomain,DC=com",
+    "OU=Supplementary,OU=PMEL Users,DC=yourdomain,DC=com"
+]
